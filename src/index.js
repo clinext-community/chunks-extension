f3859a8a719e91327ce6ff271c5af9fc72b750a2
Aboubacar Doucoure <yelounak@gmail.com>
2024-02-14 14:44:06 +0100

    feat: first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+export default ({
+  id: "@clinext-community/chunks-extension",
+  description: "Adds chunks to CliNext",
+  register: async ({ toolbox }) => {
+    return {}
+  },
+  questions: [],
+  validators: [],
+  transformers: {
+    in: [],
+    out: [],
+    display: [],
+  },
+})
