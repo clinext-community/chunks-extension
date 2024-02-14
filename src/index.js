0f4cd2bcc45e2eae4af06a6794b0ef66de08325e
Aboubacar Doucoure <yelounak@gmail.com>
2024-02-14 14:51:02 +0100

    fix: renmaed extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 export default ({
-  id: "@clinext-community/chunks-extension",
+  id: "@clinext/chunks-extension",
   description: "Adds chunks to CliNext",
   register: async ({ toolbox }) => {
     return {}
